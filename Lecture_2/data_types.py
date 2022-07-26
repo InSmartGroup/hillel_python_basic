1447c1afbb5e295bdfc989663e4865938a5c344a
InSmartGroup <insmartdatascience@gmail.com>
2022-07-26 22:15:04 +0300

    07/26/2022 Homework, part 1

---
+++ Lecture_2/data_types.py
@@ -0,0 +1,26 @@
+# Homework, part 1
+my_int_1 = 1
+my_int_2 = 3
+my_int_3 = 5
+my_int_4 = 7
+my_int_5 = 9
+
+print(my_int_1, type(my_int_1))
+print(my_int_2, type(my_int_2))
+print(my_int_3, type(my_int_3))
+print(my_int_4, type(my_int_4))
+print(my_int_5, type(my_int_5))
+
+
+my_str_1 = 'Hey'
+my_str_2 = 'this is'
+my_str_3 = "my first"
+my_str_4 = "homework"
+my_str_5 = 'code'
+
+print()
+print(my_str_1, type(my_str_1))
+print(my_str_2, type(my_str_2))
+print(my_str_3, type(my_str_3))
+print(my_str_4, type(my_str_4))
+print(my_str_5, type(my_str_5))
