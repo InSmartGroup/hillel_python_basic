a27cb94b0c6b9a5aa275d8ede6b4e6fb037df815
InSmartGroup <insmartdatascience@gmail.com>
2022-07-26 22:23:16 +0300

    07/26/2022 Homework, part 2

---
+++ Lecture_2/rows_and_numbers.py
@@ -0,0 +1,11 @@
+# Homework, part 2
+name = input("Please type your name: ").title()
+print(f"Hi {name}. It's nice to e-meet you.")
+
+print()
+float_num = float(input("Please type a floating point number: "))
+print(float_num)
+print(int(float_num))
+print(int(float_num ** 4))
+print(int(float_num ** 0.5))
+print(int(float_num % 2))
