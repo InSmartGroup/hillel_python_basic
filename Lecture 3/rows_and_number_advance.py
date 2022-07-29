220631408587e0ba7495d3b2e134be79d5c5d7e3
InSmartGroup <insmartdatascience@gmail.com>
2022-07-29 22:18:22 +0300

    07/29/2022 Homework, part 2

---
+++ Lecture 3/rows_and_number_advance.py
@@ -0,0 +1,4 @@
+name = input("Enter your name: ").title().strip()
+print(f"Hi {name}!")
+print(f"Your name has {len(name)} letters.")
+print(f"And here's your name written backwards: {name[::-1]}.")
