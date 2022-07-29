fad511ef82da7352cf0b58d72be4565c43da2e28
InSmartGroup <insmartdatascience@gmail.com>
2022-07-29 22:14:07 +0300

    07/29/2022 Homework, part 1

---
+++ Lecture 3/boll_type.py
@@ -0,0 +1,29 @@
+print(3 != 5)
+
+print()
+print(5 == 5)
+print(bool(5) == bool(5))
+
+print()
+print(3 != 4)
+print(3 < 4)
+
+print()
+var_1 = None
+var_2 = 7
+print(bool(var_1) == bool(var_2))
+print(bool(var_1) > bool(var_2))
+print(bool(var_1) >= bool(var_2))
+print(bool(var_1) < bool(var_2))
+print(bool(var_1) <= bool(var_2))
+print(bool(var_1) != bool(var_2))
+
+print()
+var_3 = ""
+var_4 = 10 - 1
+print(bool(var_3) == bool(var_4))
+print(bool(var_3) > bool(var_4))
+print(bool(var_3) >= bool(var_4))
+print(bool(var_3) < bool(var_4))
+print(bool(var_3) <= bool(var_4))
+print(bool(var_3) != bool(var_4))
