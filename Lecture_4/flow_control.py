663fd7cc2b761f76d9386c282fca1f329b6c8ece
InSmartGroup <insmartdatascience@gmail.com>
2022-08-04 09:27:37 +0300

    08/02/2022 Homework minor fixes

--- Lecture_4/flow_control.py
+++ Lecture_4/flow_control.py
@@ -4,20 +4,11 @@ action = input("Type +, -, /, or * to perform operation: ")
 
 list_of_actions = ['+', '-', '/', '*', '**', '//', '%']
 
-comparison_1 = ''
-comparison_2 = ''
+comparison = ''
 
 
 try:
     operand_1 = int(operand_1) if operand_1.isdigit() else float(operand_1)
-
-except Exception as error:
-    print()
-    print(f"Error: {error}.")
-    print("Please use integers or floating point numbers.")
-
-
-try:
     operand_2 = int(operand_2) if operand_2.isdigit() else float(operand_2)
 
 except Exception as error:
@@ -71,26 +62,13 @@ elif action == '%':
 
 
 if action not in list_of_actions:
-    comparison_1 = 'Unable to compare operands.'
-elif operand_1 > operand_2:
-    comparison_1 = f'It\'s greater than operand 2 by {operand_1 - operand_2}'
-elif operand_1 < operand_2:
-    comparison_1 = f'It\'s less than operand 2 by {operand_2 - operand_1}'
-elif operand_1 == operand_2:
-    comparison_1 = "Operand 1 is equal to operand 2"
-
-if action not in list_of_actions:
-    comparison_2 = 'Unable to compare operands.'
-elif operand_2 > operand_1:
-    comparison_2 = f'It\'s greater than operand 1 by {operand_2 - operand_1}'
-elif operand_2 < operand_1:
-    comparison_2 = f'It\'s less than operand 1 by {operand_1 - operand_2}'
-elif operand_2 == operand_1:
-    comparison_2 = "Operand 2 is equal to operand 1"
+    comparison = 'Unable to compare operands.'
+else:
+    comparison = f"{operand_1} {action} {operand_2}"
 
 
 print()
 print("Report:")
-print(f"\tOperand 1:\n\tType: {type(operand_1)}\n\tComparison: {comparison_1}")
+print(f"\tOperand 1:\n\tType: {type(operand_1)}\n\tComparison: {comparison}")
 print()
-print(f"\tOperand 2:\n\tType: {type(operand_2)}\n\tComparison: {comparison_2}")
+print(f"\tOperand 2:\n\tType: {type(operand_2)}\n\tComparison: {comparison}")
