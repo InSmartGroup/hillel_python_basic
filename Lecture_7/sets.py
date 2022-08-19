3c2fa8f0f4ffa4670ec1a67e7304238ac25d1077
InSmartGroup <insmartdatascience@gmail.com>
2022-08-20 01:43:07 +0300

    minor fixes

--- Lecture_7/sets.py
+++ Lecture_7/sets.py
@@ -1,20 +1,27 @@
-import itertools
+# import itertools
+#
+# input_1 = set(input("Type anything: "))
+# input_2 = set("Type anything again: ")
+#
+# letters = set()
+# digits_input_1 = set()
+# digits_input_2 = set()
+#
+# for char in itertools.chain(input_1, input_2):
+#     if char.isalpha():
+#         letters.add(char)
+#     elif char.isdigit():
+#         if char in input_1:
+#             digits_input_1.add(char)
+#         elif char in input_2:
+#             digits_input_2.add(char)
+#
+# print(letters)
+# print(digits_input_1.symmetric_difference(digits_input_2))
 
-input_1 = set(input("Type anything: "))
-input_2 = set("Type anything again: ")
+# Set 'update' method
+set_1 = {1, 3, 5, 7}
+set_2 = {2, 4, 6, 10}
 
-letters = set()
-digits_input_1 = set()
-digits_input_2 = set()
-
-for char in itertools.chain(input_1, input_2):
-    if char.isalpha():
-        letters.add(char)
-    elif char.isdigit():
-        if char in input_1:
-            digits_input_1.add(char)
-        elif char in input_2:
-            digits_input_2.add(char)
-
-print(letters)
-print(digits_input_1.symmetric_difference(digits_input_2))
+set_1.update(set_2)
+print(set_1)
