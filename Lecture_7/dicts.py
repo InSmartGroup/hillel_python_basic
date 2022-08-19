0da5b58a06b5d3fb7ae99eee1853470e7eee8ee9
InSmartGroup <insmartdatascience@gmail.com>
2022-08-20 01:27:34 +0300

    Initial push

--- Lecture_7/dicts.py
+++ Lecture_7/dicts.py
@@ -5,5 +5,16 @@ print(ascii_dict)
 
 
 # Part 2. User input
-user_input = "hOw are you doing tOdaY? 228"
-user_password = 1488
+user_input = input("Type something: ")
+user_password = int(input("Type 1-digit number: "))
+
+encoded_input = []
+
+letters = {letter: index + 1 for index, letter in enumerate('abcdefghijklmnopqrstuvwxyz')}
+
+for letter in user_input:
+    if letter in letters:
+        new_index = letters[letter] + user_password
+        encoded_input.append(list(letters.keys())[list(letters.values()).index(new_index)])
+
+print(f"Your encoded input is: {''.join(encoded_input)}")
