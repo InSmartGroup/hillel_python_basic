cd51941833875644e575864f9b37eb4896436800
InSmartGroup <insmartdatascience@gmail.com>
2022-08-20 21:59:42 +0300

    minor fixes

--- Lecture_7/sets.py
+++ Lecture_7/sets.py
@@ -1,27 +1,43 @@
-import itertools
+# import itertools
+#
+# input_1 = set(input("Type anything: "))
+# input_2 = set("Type anything again: ")
+#
+# letters = set()
+# digits_input_1 = set()
+# digits_input_2 = set()
+#
+# for char in itertools.chain(input_1, input_2):
+#     if char.isalpha():
+#         letters.add(char)
+#     elif char.isdigit():
+#         if char in input_1:
+#             digits_input_1.add(char)
+#         elif char in input_2:
+#             digits_input_2.add(char)
+#
+# print(letters)
+# print(digits_input_1.symmetric_difference(digits_input_2))
+#
+# # Set 'update' method
+# set_1 = {1, 3, 5, 7}
+# set_2 = {2, 4, 6, 10}
+#
+# set_1.update(set_2)
+# print(set_1)
 
-input_1 = set(input("Type anything: "))
-input_2 = set("Type anything again: ")
+# Useful set operators
+set_3 = {1, 2, 3, 5, 6, 7}
+set_4 = {2, 4, 6, 7, 8, 9}
 
-letters = set()
-digits_input_1 = set()
-digits_input_2 = set()
+# Union
+print(set_3 | set_4)
 
-for char in itertools.chain(input_1, input_2):
-    if char.isalpha():
-        letters.add(char)
-    elif char.isdigit():
-        if char in input_1:
-            digits_input_1.add(char)
-        elif char in input_2:
-            digits_input_2.add(char)
+# Intersection
+print(set_3 & set_4)
 
-print(letters)
-print(digits_input_1.symmetric_difference(digits_input_2))
+# Difference
+print(set_3 - set_4)
 
-# Set 'update' method
-set_1 = {1, 3, 5, 7}
-set_2 = {2, 4, 6, 10}
-
-set_1.update(set_2)
-print(set_1)
+# Symmetric difference
+print(set_3 ^ set_4)
