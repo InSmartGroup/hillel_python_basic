3a8a76d2e5565815770ffd478a5dd10c80fb60ed
InSmartGroup <insmartdatascience@gmail.com>
2022-08-20 14:56:00 +0300

    initial push

--- Lecture_6/lists_and_tuples.py
+++ Lecture_6/lists_and_tuples.py
@@ -6,19 +6,19 @@ print(words_list[2::3])
 
 
 # Homework, part 2. Lists comprehension
-inbound_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
+my_list = [1, 2.1, "f", "2", 3, "1", 18, "df"]
 
 out_list = [i if type(i) == float
             else i if (type(i) == int and i % 2 == 0)
             else i ** 2 if (type(i) == int and i % 2 != 0)
             else (str(int(i) * 3)) if (type(i) == str and i.isdigit())
-            else -1 for i in inbound_list]
+            else -1 for i in my_list]
 print(out_list)
 
 
 # Homework, part 2. Using for loop
 outbound_list = []
-for item in inbound_list:
+for item in my_list:
     try:
         if isinstance(item, float):
             outbound_list.append(item)
