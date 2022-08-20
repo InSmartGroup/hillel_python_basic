15d64573a10b581c9b30c9ad6c9e5d9dd3ea4154
InSmartGroup <insmartdatascience@gmail.com>
2022-08-20 14:54:30 +0300

    added another version of code

--- Lecture_7/dicts.py
+++ Lecture_7/dicts.py
@@ -4,7 +4,7 @@ ascii_dict = {i: chr(i) for i in range(0, 128)}
 print(ascii_dict)
 
 
-# Part 2. User input
+# Part 2. User input. Version 1
 user_input = input("Type something: ")
 user_password = int(input("Type 1-digit number: "))
 
@@ -17,4 +17,25 @@ for letter in user_input:
         new_index = letters[letter] + user_password
         encoded_input.append(list(letters.keys())[list(letters.values()).index(new_index)])
 
-print(f"Your encoded input is: {''.join(encoded_input)}")
+print(f"Encoded word: {''.join(encoded_input)}")
+
+
+# Part 2. User input. Version 2
+user_input = input("Type something: ")
+user_password = int(input("Enter the shift code: "))
+
+letters = {letter: index + 1 for index, letter in enumerate('abcdefghijklmnopqrstuvwxyz')}
+
+keys = list(letters.keys())
+
+values = list(letters.values())
+
+new_word = []
+
+for letter in user_input:
+    if letter in keys:
+        new_position = keys.index(letter) + user_password
+        new_letter = keys[new_position]
+        new_word.append(new_letter)
+
+print(f"Encoded word: {''.join(new_word)}")
