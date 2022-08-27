742b8b674ff27d0c54a4dcc076a7ab15f1a43c9d
InSmartGroup <insmartdatascience@gmail.com>
2022-08-27 17:47:53 +0300

    initial commit

---
+++ Lecture_10/virtual_env.py
@@ -0,0 +1,10 @@
+# Here are the steps that I used to create a venv using terminal
+
+# 1. mkdir py_venv
+# 2. cd py_benv
+# 3. python -m venv CV_venv
+# 4. cd CV_venv
+# 5. cd Scripts
+# 6. activate
+# 7. pip install opencv-contrib-python seaborn numpy matplotlib pandas keras tensorflow torch yolov5
+# 8. deactivate
